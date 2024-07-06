9f984813bf28fddb9b4709f915db240563fdad78
Ilan Gibson <ilanjgibson@gmail.com>
2024-07-05 20:03:49 -0600

    change main to threading model to allow for raw mode. ensure proper re-enabling of raw mode before starting new command line prompt. modify threading model to terminate input reading after the Enter key is pressed. create cli_match function to read and parse command line prompt into appropriate implementation.

--- cli_tool/src/main.rs
+++ cli_tool/src/main.rs
@@ -1,8 +1,14 @@
 use clap::{Parser, Subcommand};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::{fs, io};
 use std::io::Write;
 use std::error::Error;
 use std::path::PathBuf;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent};
+use std::time::Duration;
+use std::thread;
+use std::sync::mpsc;
 
 #[derive(clap::Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -30,40 +36,99 @@ enum Commands {
     },
 }
 
+#[derive(Debug)]
+enum InputEvent {
+    Char(char),
+    Enter,
+    Tab,
+    Delete,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     loop {
         print!("\ncli_tool> ");
-        io::stdout().flush().unwrap();
+        io::stdout().flush()?;
+        let _ = enable_raw_mode();
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            loop {
+                if let Ok(Event::Key(KeyEvent { code, modifiers: _ })) = event::read() {
+                    match code {
+                        KeyCode::Char(c) => {
+                            let _ = tx.send(InputEvent::Char(c));
+                        }
+                        KeyCode::Enter => {
+                            let _ = tx.send(InputEvent::Enter);
+                            break;
+                        }
+                        KeyCode::Tab => {
+                            let _ = tx.send(InputEvent::Tab);
+                            break;
+                        }
+                        KeyCode::Delete => {
+                            let _ = tx.send(InputEvent::Delete);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        });
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("fail");
+        loop {
+            if let Ok(event) = rx.recv_timeout(Duration::from_millis(10)) {
+                match event {
+                    InputEvent::Char(c) => {
+                        input.push(c);
+                        print!("{}", c);
+                        io::stdout().flush()?;
+                    }
+                    InputEvent::Enter => {
+                        disable_raw_mode()?;
+                        print!("\n");
+                        cli_match(input.clone())?;
+                        break;
+                    }
+                    InputEvent::Tab => {
+                        break;
+                    }
+                    InputEvent::Delete => {
+                        break;
+                    }
 
-        let mut args: Vec<&str> = input.split_whitespace().collect();
-        args.insert(0, "cli_tool");
-
-        let cli = Cli::parse_from(&args);
-
-
-        match &cli.command {
-            Commands::Ls { list: _, all, path} => {
-                let default_path = PathBuf::from(".");
-                if *all {
-                    let _ = list_files(path.as_ref().unwrap_or(&default_path).clone(), Some(all));
-                } else {
-                    let _ = list_files(path.as_ref().unwrap_or(&default_path).clone(), None);
                 }
-            },
-
-            Commands::Clear {} => {
-                clear_terminal();
-            },
-
-            Commands::Exit {} => {
-                clear_terminal();
-                return Ok(());
             }
         }
     }
+}
+
+
+fn cli_match(input: String) -> Result<(), Box<dyn Error>> {
+    let mut args: Vec<&str> = input.split_whitespace().collect();
+    args.insert(0, "cli_tool");
+
+    let cli = Cli::parse_from(&args);
+
+    match &cli.command {
+        Commands::Ls { list: _, all, path} => {
+            let default_path = PathBuf::from(".");
+            if *all {
+                let _ = list_files(path.as_ref().unwrap_or(&default_path).clone(), Some(all));
+            } else {
+                let _ = list_files(path.as_ref().unwrap_or(&default_path).clone(), None);
+            }
+        },
+
+        Commands::Clear {} => {
+            clear_terminal();
+        },
+
+        Commands::Exit {} => {
+            clear_terminal();
+        }
+    }
+    return Ok(())
 }
 
 fn list_files(path: PathBuf, all: Option<&bool> ) -> Result<(), Box<dyn Error>> {
@@ -88,10 +153,44 @@ fn list_files(path: PathBuf, all: Option<&bool> ) -> Result<(), Box<dyn Error>> 
             continue;
         }
     }
-
     Ok(())
 }
 
 fn clear_terminal() {
     print!("{esc}c", esc = 27 as char);
+}
+
+fn get_suggestions(input: &str) -> Vec<String> {
+    let commands = vec![
+        "help".to_string(),
+        "exit".to_string(),
+        "lsss".to_string(),
+        "show".to_string(),
+    ];
+
+    commands.into_iter().filter(|cmd| cmd.starts_with(input)).collect()
+}
+
+fn handle_events(stdout: &mut io::Stdout) -> crossterm::Result<()> {
+    let input = String::new();
+
+
+    loop {
+        if event::poll(Duration::from_millis(500))? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Tab => {
+                        println!("input is {}", input);
+                        let suggestions = get_suggestions(&input);
+                        if !suggestions.is_empty() {
+                            writeln!(stdout, "\nSuggestions: {:?}", suggestions)?;
+                            stdout.flush().unwrap();
+                        }
+                    }
+
+                    _ => {return Ok(())}
+                }
+            }
+        }
+    }
 }
